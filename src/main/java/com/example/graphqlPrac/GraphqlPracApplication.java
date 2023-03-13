0788e9c9f0aa5191d1d0dc7ec8182b2f9d94be60
zooragi <eldehd9898@gmail.com>
2023-03-13 11:55:55 +0900

    initial commit

---
+++ src/main/java/com/example/graphqlPrac/GraphqlPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphqlPrac/GraphqlPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
